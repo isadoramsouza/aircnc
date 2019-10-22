33cb8ba3f79728bc7c936f854d408a3424f2c09d
Isadora Souza <isadora_msouza@hotmail.com>
2019-10-22 20:24:43 -0300

    finalizando front e back sem opcoes avançadas

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -23,7 +23,7 @@ module.exports = {
 
     const spot = await Spot.create({
       user: user_id,
-      thumbail: filename,
+      thumbnail: filename,
       company,
       price,
       techs: techs.split(",").map(tech => tech.trim())
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
 import React from "react";
 import "./App.css";
 import logo from "./assets/logo.svg";
-
+import Routes from "./routes";
 function App() {
   return (
     <div className="container">
       <img src={logo} alt="Aircnc"></img>
 
       <div className="content">
-        <p>
-          Ofereça <strong>Spots</strong> para programadores e encontre{" "}
-          <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form>
-          <label htmlFor="email">E-MAIL *</label>
-          <input type="email" id="email" placeholder="Seu e-mail"></input>
-          <button className="btn" type="submit">
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
