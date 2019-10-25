78ceff6d3e7e48010e3381473fe3eeae1b73c88b
Isadora Souza <isadora_msouza@hotmail.com>
2019-10-25 20:06:29 -0300

    finalizando

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState, useMemo } from "react";
-import api from "../../services/api";
 import { Link } from "react-router-dom";
 import socketio from "socket.io-client";
+import api from "../../services/api";
+
 import "./styles.css";
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
   const [requests, setRequests] = useState([]);
+
   const user_id = localStorage.getItem("user");
 
   const socket = useMemo(
@@ -24,7 +26,7 @@ export default function Dashboard() {
   }, [requests, socket]);
 
   useEffect(() => {
-    async function loadSpot() {
+    async function loadSpots() {
       const user_id = localStorage.getItem("user");
       const response = await api.get("/dashboard", {
         headers: { user_id }
@@ -33,18 +35,21 @@ export default function Dashboard() {
       setSpots(response.data);
     }
 
-    loadSpot();
+    loadSpots();
   }, []);
 
   async function handleAccept(id) {
     await api.post(`/bookings/${id}/approvals`);
+
     setRequests(requests.filter(request => request._id !== id));
   }
 
   async function handleReject(id) {
     await api.post(`/bookings/${id}/rejections`);
+
     setRequests(requests.filter(request => request._id !== id));
   }
+
   return (
     <>
       <ul className="notifications">
@@ -76,10 +81,11 @@ export default function Dashboard() {
           <li key={spot._id}>
             <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
             <strong>{spot.company}</strong>
-            <span>{spot.price ? `R$${spot.price}/dia` : `GRATUITO`}</span>
+            <span>{spot.price ? `R$${spot.price}/dia` : "GRATUITO"}</span>
           </li>
         ))}
       </ul>
+
       <Link to="/new">
         <button className="btn">Cadastrar novo spot</button>
       </Link>
